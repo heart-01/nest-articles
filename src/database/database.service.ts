dcb219b07907f3f2b574f10089cf350172e28aed
siwat jermwatthana <siwat.j@hiveground.com>
2024-01-08 01:27:15 +0700

    [Feat]: Add logging middleware to DatabaseService

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,9 +1,29 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient, Prisma } from '@prisma/client';
 
 @Injectable()
-export class DatabaseService extends PrismaClient implements OnModuleInit {
+export class DatabaseService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit() {
     await this.$connect();
+    this.$use(this.loggingMiddleware);
+  }
+
+  loggingMiddleware: Prisma.Middleware = async (params, next) => {
+    console.log(
+      `${params.action} ${params.model} ${JSON.stringify(params.args)}`,
+    );
+
+    const result = await next(params);
+
+    console.log(result);
+
+    return result;
+  };
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
