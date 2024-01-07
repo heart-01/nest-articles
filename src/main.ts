3403452d85d89172c6a9381073a0e03d2df8b9ac
siwat jermwatthana <siwat.j@hiveground.com>
2024-01-08 00:43:34 +0700

    [Feat]: Add global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { setupSwagger } from './config/swagger.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   const configService = app.get(ConfigService);
   const port = configService.getOrThrow('PORT');
